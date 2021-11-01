a054fa379e7e58e7a01554c4fda06a7c579e5452
Kemuel12 <73594961+Kemuel12@users.noreply.github.com>
2021-11-01 21:07:50 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,83 @@
+status=""
+object=[]
+
+function preload()
+{
+   img= loadImage("bedroom.jpg");
+}
+
+function setup()
+{
+   canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Bedroom";
+}
+
+status =""
+function preload()
+{
+    img=loadImage("bedroom.jpg");
+}
+
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects"
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    else
+    {
+        console.log(results);
+        object=results;
+    }
+
+}
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#b50000");
+    text("Bedroom",70,100);
+    noFill();
+    stroke("#ff0000");
+    rect(45,70,300,330);
+
+}
+
+function back()
+{
+    window.location="index.html";
+}
+
+
+if (status !="")
+
+{
+    for(i=0;i<object.length;i++)
+ {
+     document.getElementById("status").innerHTML="Status: Objects Detected"
+
+     fill("#b50000")
+     percent=floor(object[i].confidence*100)
+     text()
+
+     
+ }
+}
+ 
